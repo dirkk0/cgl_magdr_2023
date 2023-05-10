a8e2323630ca500fdf199db9aa66c35f0ddf3af7
Dirk Krause <dirkk0@googlemail.com>
2023-05-10 18:48:09 +0200

    v

--- first_test/MeshInstance6.gd
+++ first_test/MeshInstance6.gd
@@ -5,6 +5,8 @@ extends MeshInstance
 # var a = 2
 # var b = "text"
 
+var d = 0
+var speed = 0.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,4 +16,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	self.translation = Vector3(2, 0, 0)
+	# self.translation = self.translation - Vector3(speed*delta, 0, 0)
+	self.translation.x -= speed*delta
+#	
+#	d += delta
+#   self.translation = Vector3(d * speed, 1, 0)
