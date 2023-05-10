5606d3b94ff5c5988bee63336d43dab4ae38d48c
Dirk Krause <dirkk0@googlemail.com>
2023-05-10 20:05:32 +0200

    v

--- second_test/main.gd
+++ second_test/main.gd
@@ -15,7 +15,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	if Input.is_action_just_pressed("ui_a"):
-		print("aaa")
-	if Input.is_action_just_pressed("ui_w"):
-		print("www")
+
