808980b6355d7f3e9a548c127af0c022f171c306
Dirk Krause <dirkk0@googlemail.com>
2023-05-24 18:10:57 +0200

    v

---
+++ 3_scene_change/scene_outro.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_pressed():
+	var intro_scene = load("res://scene_intro.tscn")
+	get_tree().change_scene_to(intro_scene)
+	pass # Replace with function body.
