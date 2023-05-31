0aa02790a28987363c2601abc5c2857818af1409
Dirk Krause <dirkk0@googlemail.com>
2023-05-31 16:25:26 +0200

    v

---
+++ 3_scene_change/assets/player/agents/agent.gd
@@ -0,0 +1,34 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var idx = -1
+
+var waypoints = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	waypoints = get_tree().get_nodes_in_group("waypoints")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Timer_timeout():
+	print("works")
+	
+	idx = fmod(idx +1, waypoints.size() )
+	
+	var pos = waypoints[idx].translation
+
+	
+	# var x = rand_range(0,10)
+	# var z = rand_range(0, 10)
+	translation = pos
+	pass # Replace with function body.
