0fc4461cb2a9cd35c502024f0985acf4e674dcdd
Dirk Krause <dirkk0@googlemail.com>
2023-10-04 20:13:49 +0200

    Update main.js

--- programming1o3/main.js
+++ programming1o3/main.js
@@ -4,6 +4,8 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 const scene = new THREE.Scene();
+
+scene.background = new THREE.Color(0x88ccee)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
