edffdcdeced820dd230cc9adc861bc626114057f
Dirk Krause <dirkk0@googlemail.com>
2023-10-04 19:33:25 +0200

    v

--- programming1o3/main.js
+++ programming1o3/main.js
@@ -1,26 +1,38 @@
+
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+document.body.appendChild(renderer.domElement);
+// document.querySelector("#test").appendChild(renderer.domElement)
+
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "yellow", wireframe: true });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 camera.position.z = 5;
 
-function animate() {
-	requestAnimationFrame( animate );
+// setInterval(() => {
+//     // console.log("does it?")
+//     cube.rotation.x += 0.01;
+//     cube.rotation.y += 0.01;
+//     renderer.render(scene, camera);
+// }, 1);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
 
-	renderer.render( scene, camera );
+function update() {
+    requestAnimationFrame( update );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
 }
 
-animate();
+update();
