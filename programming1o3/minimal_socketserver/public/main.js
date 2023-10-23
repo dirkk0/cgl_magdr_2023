8b78cdc6c8a149cdf17a9be0805b5e3c13a392da
Dirk Krause <dirkk0@googlemail.com>
2023-10-23 16:04:29 +0200

    minimal socketserver

---
+++ programming1o3/minimal_socketserver/public/main.js
@@ -0,0 +1,126 @@
+"use strict";
+
+const FREQ = 33;
+let world = [0, 0, 0];
+
+function NetObject() {
+  this.users = [];
+
+  this.init = function () {
+    console.log("net init", this.users);
+
+    this.socket = io();
+    this.socket.on("connect", () => {
+      console.log("yay! My id is ", this.socket.id);
+    });
+
+    this.sendData = function (that) {
+      let msg = {
+        posrot: that.posrot,
+      };
+      this.socket.emit("data-to-server", msg);
+    };
+    this.socket.on("outro", (key) => {
+      console.log(key, "is gone.");
+      delete this.users[key];
+
+      let thisEl = document.querySelector("#player-" + key);
+      if (thisEl) {
+        thisEl.parentNode.removeChild(thisEl);
+      }
+
+      //   let thisEl = document.querySelector("#rig_" + key);
+      //   if (thisEl) {
+      //     thisEl.parentNode.removeChild(thisEl);
+      //   }
+    });
+
+    this.socket.on("world-to-client", (w) => {
+      // console.log("world", world)
+      world = w;
+
+      return;
+    });
+
+    this.socket.on("data-to-client", (m) => {
+      // if not me
+      if (m.id != this.socket.id) {
+        // if not in dictionary
+        if (!netObject.users[m.id]) {
+          // create a new player
+          console.log("new user", m.id);
+          netObject.users[m.id] = {};
+          console.log("other users", netObject.users);
+
+          let players = document.getElementById("container");
+          players.innerHTML += `<div class="players" id="player-${m.id}">-${m.id}<div>`;
+        }
+        netObject.users[m.id].posrot = m.posrot;
+      }
+    });
+  };
+}
+
+window.netObject = new NetObject();
+netObject.init();
+
+let myself = {};
+
+let users = netObject.users;
+
+setInterval(() => {
+  // iterate through dictinary (which is not an array)
+  Object.keys(users).forEach((key) => {
+    // if not me
+    if (key != netObject.socket.id) {
+      const u = users[key];
+      let id = "player-" + key;
+      let player = document.getElementById(id);
+      // has coordinates
+      if (u.posrot) {
+        let x = 200 + u.posrot[0] * 10;
+        let y = 200 + u.posrot[1] * 10;
+
+        // move the other players
+        player.style.top = x + "px";
+        player.style.left = y + "px";
+      }
+    }
+  });
+}, FREQ);
+
+setInterval(() => {
+  // send my position
+  netObject.sendData(myself);
+}, FREQ);
+
+setInterval(() => {
+  // just monitor the coordinates, not really needed
+  info.innerText = JSON.stringify({ ...netObject.users });
+}, 500);
+
+
+function doInteract(msg) {
+  // global world object update, like a click on a cube
+  world[2] += 1;
+  netObject.socket.emit("world-to-server", world);
+}
+
+setInterval(() => {
+  // create random position every second, so we see some changes
+  let pos, rot;
+
+  let rx = Math.random() * 10;
+  let ry = Math.random() * 10;
+  pos = { x: rx, y: ry, z: 3 };
+  rot = { x: 0, y: 0, z: 0 };
+  myself.posrot = [
+    pos.x.toFixed(2),
+    pos.y.toFixed(2),
+    pos.z.toFixed(2),
+    rot.x.toFixed(2),
+    rot.y.toFixed(2),
+    rot.z.toFixed(2),
+    0,
+  ];
+}, 1000);
---
+++ programming1o3/minimal_socketserver/server.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const fs = require('fs');
+const http = require('http');
+// const https = require('https');
+const express = require('express');
+
+const socketio = require('socket.io');
+
+
+
+const PORT = process.env.PORT || 5002;
+
+
+let app = express();
+app.use('/', express.static(__dirname + '/public'));
+
+const server = http.createServer(app)
+// const server = https.createServer(credentials, app)
+
+const io = socketio(server);
+
+let startTime = new Date()
+let serverTime = null
+server.listen(PORT, () =>  {
+    console.log(`Server running on port ${PORT}`)
+})
+
+
+
+let world = [0, 0, 1]
+
+io.on('connection', (socket) => {
+    console.log("LOG: connected with ", socket.id, socket.handshake.address);
+
+    socket.on('data-to-server', (msg) => {
+
+        msg.id = socket.id
+        io.emit('data-to-client', msg);
+    });
+
+    socket.on('world-to-server', (w) => {
+        world = w
+    });
+
+    socket.on('disconnect', (c) => {
+        console.log('LOG:  disconnect: ' + c + socket.id);
+        // console.log(socket.id)
+        io.emit('outro', socket.id);
+    });
+
+});
+
+// setInterval(() => {
+//     let temp = ""
+//     for (const [_, socket] of io.of("/").sockets) {
+//         if (socket.username != "monitor") {
+//             // console.log(Object.keys(socket.rooms))
+//             temp += "\n" + socket.id + " " + socket.username + " " + socket.prop_room + " " + socket.prop_x + " " + socket.prop_y
+//         }
+//     }
+//     let today = new Date();
+//     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//     io.emit('monitorinfo', temp);
+//     // io.to("abc123").emit('info1', "Hello room abc123, it's "+ time);
+//     io.to("def456").emit('info1', "Hello room def, it's " + time);
+// }, 500);
+
+
+setInterval(() => {
+    serverTime = new Date() - startTime
+    world[0] = (serverTime / 1000).toFixed(1)
+    io.emit('world-to-client', world);
+    // console.log(world)
+}, 500);
